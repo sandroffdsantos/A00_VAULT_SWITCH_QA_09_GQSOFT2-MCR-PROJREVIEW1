0a41b218ab3d4e5eecea3f4c388c79d542163bee
Sandro Santos [1222645] <1222645@isep.ipp.pt>
2023-10-11 19:55:07 +0100

    pre-commit hook test

--- src/tax/tax-calc.ts
+++ src/tax/tax-calc.ts
@@ -2,10 +2,12 @@ export class TaxesCalculation {
   public calculateTaxGB(): number {
     // ...
     return 1;
-  }  public calculateTaxUSA(): number {
+  }
+  public calculateTaxUSA(): number {
     // ...
     return 2;
-  }  public calculateTaxPT(): number {
+  }
+  public calculateTaxPT(): number {
     // ...
     return 3;
   }
