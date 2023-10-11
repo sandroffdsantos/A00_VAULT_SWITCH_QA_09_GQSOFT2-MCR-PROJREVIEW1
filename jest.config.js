327e925badac9dde034fe9c9bbd13ba99a0a61ca
Sandro Santos [1222645] <1222645@isep.ipp.pt>
2023-10-11 18:44:56 +0100

    hook setup

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports={
-    "roots":["src"],
-    "transform": {"^.+\\.(ts|tsx)?$": "ts-jest"},
-}
+module.exports = {
+  roots: ["src"],
+  transform: { "^.+\\.(ts|tsx)?$": "ts-jest" },
+};
--- src/tax/tax-calc.ts
+++ src/tax/tax-calc.ts
@@ -1,14 +1,14 @@
 export class TaxesCalculation {
-public calculateTaxGB(): number {
-// ...
-return 1;
-}
-public calculateTaxUSA(): number {
-// ...
-return 2;
-}
-public calculateTaxPT(): number {
-// ...
-return 3;
-}
+  public calculateTaxGB(): number {
+    // ...
+    return 1;
+  }
+  public calculateTaxUSA(): number {
+    // ...
+    return 2;
+  }
+  public calculateTaxPT(): number {
+    // ...
+    return 3;
+  }
 }
