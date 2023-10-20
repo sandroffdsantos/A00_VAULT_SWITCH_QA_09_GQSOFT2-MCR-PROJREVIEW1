2e83188bc5d61ffc816e699aeb6b4659f8c0db6a
Sandro Santos [1222645] <1222645@isep.ipp.pt>
2023-10-20 12:01:50 +0100

    test AI set-up

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 /** @format */
 
-                import { sumOnly } from "./domain/calc";
+                        import { sumOnly } from "./domain/calc";
 
-                let printMessage = (msg: string): void => console.log(`Mensagem: ${msg}`);
+                        let printMessage = (msg: string): void => console.log(`Mensagem: ${msg}`);
 
-                let msg = "Olá Switch-QA";
+                        let msg = "Olá Switch-QA";
 
-                printMessage(msg);
+                        printMessage(msg);                        let list: number[] = [1, 2, 3, 4];
 
-                let list: number[] = [1, 2, 3, 4];
-
-                let total = sumOnly(...list);
-printMessage(total.toString());
+                        let total = sumOnly(...list);
+                        printMessage(total.toString());
 
 // student one: please replace by console.log
 // student two: add to calc.ts a function sumWithDiscount(disc, valores)
