767b3ec3cca1f0c4890a157f07b490bfbc600c93
Sandro Santos [1222645] <1222645@isep.ipp.pt>
2023-10-22 00:59:51 +0100

    changed calc.test.ts file

--- src/test/calc.test.ts
+++ src/test/calc.test.ts
@@ -2,7 +2,7 @@
 
 import { sumOnly } from "../domain/calc";
 
-test("verifica resultado", () => {
+test("verifica resultado da soma", () => {
   let result = sumOnly(1, 2, 3, 4, 5);
 
   expect(result).toBe(15);
